6c72c8d9952a068843dd8623dad32b0b2fc2b53c
SAYEED05 <sayeed.705@gmail.com>
2021-08-01 09:27:43 +0530

    show error message for not filling all required fields

--- src/microComponents/Summary.js
+++ src/microComponents/Summary.js
@@ -4,9 +4,18 @@ const Summary = ({ additionalCost, added, shortDetails, checkerFunction }) => {
   const price = shortDetails.map((item) => item.basePrice);
   const basePrice = parseInt(price);
   const [canAdd, setCanAdd] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const addItemHandler = () => {
-    canAdd && window.alert("added");
+    if (canAdd) {
+      setShowError(false);
+      window.alert("added");
+    } else {
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 2000);
+    }
   };
 
   useEffect(() => {
@@ -16,30 +25,37 @@ const Summary = ({ additionalCost, added, shortDetails, checkerFunction }) => {
   }, [added]);
 
   return (
-    <div className="summary-wrapper">
-      <div className="summary-content">
-        <div className="add-ons-details">
-          <div className="add-on-full" style={{ display: "flex" }}>
-            {added.slice(0, added.length > 1 ? 2 : 1).map((item, index) => (
-              <div key={index}>{item.name},</div>
-            ))}
-          </div>
-          <div className="add-on-short">
-            {added.length > 2 && `+${added.length - 2}  Add On`}
-          </div>
+    <>
+      {showError && (
+        <div className="show-error">
+          <div className="error-content">Select all the required fields</div>
         </div>
-        <div
-          className="add-ons-total-strip"
-          onClick={addItemHandler}
-          style={{ background: canAdd ? "#60b246" : "grey" }} //CHANGING THE COLOR BASED ON CHECKER FUNCTIONS
-        >
-          <div className="total-amount">
-            Total ₹{basePrice + additionalCost}
+      )}
+      <div className="summary-wrapper">
+        <div className="summary-content">
+          <div className="add-ons-details">
+            <div className="add-on-full" style={{ display: "flex" }}>
+              {added.slice(0, added.length > 1 ? 2 : 1).map((item, index) => (
+                <div key={index}>{item.name},</div>
+              ))}
+            </div>
+            <div className="add-on-short">
+              {added.length > 2 && `+${added.length - 2}  Add On`}
+            </div>
           </div>
-          <div className="add-item-cta">ADD ITEM</div>
+          <div
+            className="add-ons-total-strip"
+            onClick={addItemHandler}
+            style={{ background: canAdd ? "#60b246" : "grey" }} //CHANGING THE COLOR BASED ON CHECKER FUNCTIONS
+          >
+            <div className="total-amount">
+              Total ₹{basePrice + additionalCost}
+            </div>
+            <div className="add-item-cta">ADD ITEM</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
